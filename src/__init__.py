9c1a21e067b5cf7a518b271e5734eb7769bb7b75
Upendra Upadhyay <upendra.upadhyay.97@gmail.com>
2018-01-23 01:41:36 +0530

    removed hasura examples.

--- src/__init__.py
+++ src/__init__.py
@@ -10,7 +10,7 @@ WTF_CSRF_ENABLED = True
 
 # This line adds the hasura example routes form the hasura.py file.
 # Delete these two lines, and delete the file to remove them from your project
-from .hasura import hasura_examples
-app.register_blueprint(hasura_examples)
+# from .hasura import hasura_examples
+# app.register_blueprint(hasura_examples)
 
 from .views import *
